4dc26f5f856f1fad417010c52153074d8ce9ee0e
Abdiel "A.B." Villalobos <ablobosportfolio@gmail.com>
2022-12-12 20:55:43 -0500

    fine tuning

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,3 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 const events = document.querySelectorAll('.event')
 const now = setInterval(update, 1000);
 
